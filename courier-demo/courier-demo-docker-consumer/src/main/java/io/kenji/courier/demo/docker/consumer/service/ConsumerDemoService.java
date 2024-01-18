8b87655d1da62512cb5e88436a55ec13d5da71fe
Kenji Peng <kenjipan@foxmail.com>
2024-01-18 21:48:18 +0800

    Docker integration in RPC consumer

---
+++ courier-demo/courier-demo-docker-consumer/src/main/java/io/kenji/courier/demo/docker/consumer/service/ConsumerDemoService.java
@@ -0,0 +1,12 @@
+package io.kenji.courier.demo.docker.consumer.service;
+
+/**
+ * @Author Kenji Peng
+ * @Description
+ * @Date 2024-01-10
+ **/
+public interface ConsumerDemoService {
+
+    String hello(String name);
+
+}
